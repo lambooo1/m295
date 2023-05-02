2f714c42a9e3331ff8286383de498a80561c1403
lambooo1 <logendranl@bzz.ch>
2023-05-02 14:58:33 +0000

    task moodle

--- express_helloworld/index.js
+++ express_helloworld/index.js
@@ -2,9 +2,23 @@ import express from "express";
 
 const app = express(); 
 const port = 3000
+const names = ["Abir", "Arham", "Sadi", "Labi", "Mahdi"]
+
 
 app.get("/", (req, res) => {
     res.send("Hello World")
+})
+
+app.get("/now", (req, res) => {
+    res.send(new Date().toISOString())
+})
+
+app.get("/zli", (req, res) => {
+    res.redirect("https://moodle.zli.ch/")
+})
+
+app.get("/name", (req, res) => {
+    res.send(names[Math.floor(Math.random * names.length)])
 })
 
 app.listen(port, () => {

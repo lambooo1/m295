6618683149823a4d8fe0d8274694b9489b4acfaa
lambooo1 <logendranl@bzz.ch>
2023-05-02 09:06:20 +0000

    fix my-fist-async-io task

--- my-first-async-io.js
+++ my-first-async-io.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
-
-fs.readFile(process.argv[2], function doneReading(err, fileContents) {
-    if err throw err;
+fs.readFile(process.argv[2], function(err, data) {
+    if (err) return console.error(err);
+    const buff = fs.readFileSync(process.argv[2]);
     const str = buff.toString();
     const lines = str.split('\n').length-1;
     console.log(lines);   

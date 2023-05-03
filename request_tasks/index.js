3a256d27d06a1eddbb93383257f4f69c0a646e18
lambooo1 <logendranl@bzz.ch>
2023-05-03 14:34:06 +0000

    update: Post-request Add: Delete-request

--- request_tasks/index.js
+++ request_tasks/index.js
@@ -1,19 +1,16 @@
 import express, { request } from "express";
-//import cors from "cors"; 
+import cors from "cors"; 
+import multer from "multer"; 
 
 const app = express(); 
-const port = 3050
-const names = ['Hans', 'Bart', 'Lisa']
+const port = 3065 
 const postname = "/workspaces/m295/request_tasks/postname.html" 
+let names = ['Hans', 'Bart', 'Lisa', 'Lambo', 'Nikola'] 
 
+app.use(cors());
 app.use(express.urlencoded());
-//app.use(cors()); 
-
-/*
-app.get("/", (req, res) => {
-    res.send("Hello World")
-})
-*/
+app.use(express.json());
+//app.use(express.static("request_tasks" + "/public")); 
 
 app.get("/now", (req, res) => {
     const tz = req.query.tz || "Europe/Berlin";
@@ -22,17 +19,35 @@ app.get("/now", (req, res) => {
     //url http://localhost:3000/now?tzuct
 })
 
-app.get('/', function(request, response, next){
-	response.sendFile(postname);
+app.get('/', (req, res) => {
+	res.send("Hello World");
 });
 
-app.post('/', function(request, response, next){
-	response.send(request.body);
+app.get('/name', (req, res) => {
+	res.sendFile(postname);
+});
+  
+app.post('/names-post', (req, res) => {
+    let name = req.body.namepost;
+    names.push(name); 
+	res.send(names);
 });
 
-app.delete('/name', (res, req) => {
-    console.log(req.body)
-})
+app.delete('/namesdel', multer().none(), (req, res) => {
+    console.log(req.body.namedelete);
+    names = names.filter((n) => n !== req.body.name);
+    console.log(names);
+    res.sendStatus(204);
+  });
+
+app.get('/secret2', (request, response) => {
+    const info = req.get(Headers)
+	if (info == "BasicaGFja2VyOjEyMzQ="){
+        res.sendStatus(201);
+    }else {
+        res.sendStatus(401); 
+    }
+});
 
 app.listen(port, () => {
     console.log("Server ist gestartet.");

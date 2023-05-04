353f9f2001761bda7834fb9c5d8e1509f0997cff
lambooo1 <logendranl@bzz.ch>
2023-05-04 18:13:28 +0000

    add: set/get/delete session name

--- authentification/index.js
+++ authentification/index.js
@@ -4,24 +4,51 @@ import session from "express-session";
 const app = express()
 const port = 3080
 
+app.use(express.json());
 app.use(session({
     secret: 'supersecret',
-    resave: false, 
-    saveUninitialized: true, 
-    cookie: {} 
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
 }))
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Something went wrong!');
+});
 
 //const secretAdminCredentials = { email: "desk@library.example", password: "m295" }
 
 app.get("/", (req, res, _) => {
-    req.session.views = (req.session.views || 0) + 1  
-    console.log(req.session) 
+    req.session.views = (req.session.views || 0) + 1
+    console.log(req.session)
 
-    res.end(req.session.views + 'views') 
+    res.end(req.session.views + 'views')
 })
 
 app.post("/name", (req, res) => {
-    const value = req.query.cookie
+    const name = req.query?.name
+    req.session.name = name
+    res.send(`Name ${name} erfolgreich gespeichert!`)
+})
+
+app.get('/name', (req, res) => {
+    const name = req.session?.name;
+    if (!name) {
+        res.status(404).send('No name found in session.');
+        return;
+    }
+    res.send(`Name in session: ${name}`);
+});
+
+app.delete("/name", (req, res) => {
+    const name = req.session?.name
+    if (!name) {
+        res.status(404).send('No name found in session.');
+    } else {
+        req.session.name = null
+        res.send(`${name} wurde gelöscht!`)
+    }
 })
 
 /*
@@ -47,9 +74,9 @@ app.get("/verify", (req, res) => {
 
 app.delete("/logout", (req, res) => {
     if (request.session.email) {
-		request.session.email = null
-		return response.status(204).send()
-	}
+        request.session.email = null
+        return response.status(204).send()
+    }
   return response.status(401).json({ error: "Not logged in" })
 })
 */
